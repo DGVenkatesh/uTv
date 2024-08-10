98c0ba45d953a7a5d84c062cae07d8ffd4be5908
DGVenkatesh <dgvenkatesh08@gmail.com>
2024-08-10 22:52:30 +0530

    feat: init v2

---
+++ app/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, SafeAreaView, Dimensions, StatusBar } from 'react-native';
+import { Video } from 'expo-av';
+
+const App = () => {
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar hidden />
+      <Video
+        source={{ uri: 'http://cloudstream4k.com:1935/utv/live/playlist.m3u8' }} // Replace with your video URL
+        style={styles.fullScreen}
+        useNativeControls
+        resizeMode="stretch" // You can adjust the resizeMode as needed (e.g., 'cover', 'stretch')
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  fullScreen: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
+
+export default App;
